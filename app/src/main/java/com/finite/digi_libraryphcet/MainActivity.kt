1fa7037670f8f20de06386770e1c69df15bb3c02
Fin1te <rishabhmehta00@gmail.com>
2022-02-26 19:30:43 +0530

    Initial commit

---
+++ app/src/main/java/com/finite/digi_libraryphcet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.finite.digi_libraryphcet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
